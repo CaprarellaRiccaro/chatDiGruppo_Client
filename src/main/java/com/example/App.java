3993c76d25ff42cfd6cb3349e1306c5be0f776f8
lai2-24 <lai2-24@itismeucci.com>
2023-11-20 10:06:14 +0100

    Il Client invia una Stringa

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,40 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        try{
+            Socket mioSocket = new Socket("localhost", 6789);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            BufferedReader inDalServer = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+            DataOutputStream outVersoServer = new DataOutputStream(mioSocket.getOutputStream());
+
+            String stringaServer;
+            String st;
+            
+            do{
+                System.out.println("Digita un messaggio oppure EXIT per uscire");
+                st = in.readLine();    
+                outVersoServer.writeBytes(st + '\n');
+                stringaServer = inDalServer.readLine();
+                System.out.println( stringaServer );
+                if(st.equals("EXIT")){
+                    mioSocket.close();
+                }
+            } while (true);
+        }
+        
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante la comunicazione!");
+            System.exit(1);
+        }
     }
 }
